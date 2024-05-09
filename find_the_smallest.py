92df9bbfc6c77bc1d0de87a2d9dbf6c16eb402e1
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-05-09 22:49:53 +0300

    09.05.2024

--- find_the_smallest.py
+++ find_the_smallest.py
@@ -1,44 +1,21 @@
-def secondary_check(current_number):
-    same_number_index_list = []
-    current_number = str(current_number)
-    comparison_digi = int(current_number[0])
-    smallest_so_far = None
-
-    for index, same_digi in enumerate(current_number):
-        if int(same_digi) == comparison_digi:
-            same_number_index_list.append(index)
-
-    temp_number = [n for n in str(current_number)]
-
-    for num_in_list in same_number_index_list:
-        n = temp_number.pop(num_in_list)
-        temp_number.insert(0, n)
-        temp_number = int("".join(temp_number))
-        if temp_number < int(current_number):
-            smallest_so_far = temp_number
-        continue
-
-    if smallest_so_far:
-        return smallest_so_far
-    return current_number
-
-
 def smallest(n):
-    smallest_digi = 9
-    smallest_digi_index = None
+    number_as_str_list = [n for n in str(n)]
+    num_copy = number_as_str_list.copy()
 
-    current_number = [n for n in str(n)]
-    for index, num in enumerate(current_number):
-        if int(num) < smallest_digi:
-            smallest_digi = int(num)
-            smallest_digi_index = index
-    current_number.pop(smallest_digi_index)
-    current_number.insert(0, str(smallest_digi))
-    current_number = int("".join(current_number))
+    smallest_single_digi = min([int(n) for n in number_as_str_list])
 
-    final = secondary_check(current_number)
+    temp_smallest = n
 
-    return final
+    for index, digi in enumerate(number_as_str_list):
+        if int(digi) == smallest_single_digi:
+            extract = num_copy.pop(index)
+            num_copy.insert(0, extract)
+            new_value_as_int = int("".join(num_copy))
+            if new_value_as_int < n:
+                temp_smallest = new_value_as_int
+            num_copy = number_as_str_list
+
+    return temp_smallest
 
 
 print(smallest(62524))
