c5fe2f5a8b52d45a6e1e8ed5e8559700bcb8eb7f
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-05-09 22:12:13 +0300

    09.05.2024

--- find_the_smallest.py
+++ find_the_smallest.py
@@ -1,2 +1,17 @@
 def smallest(n):
-    pass
+    smallest_digi = 9
+    smallest_digi_index = None
+
+    current_number = [n for n in str(n)]
+    for index, num in enumerate(current_number):
+        if int(num) < smallest_digi:
+            smallest_digi = int(num)
+            smallest_digi_index = index
+    current_number.pop(smallest_digi_index)
+    current_number.insert(0, str(smallest_digi))
+    current_number = int("".join(current_number))
+
+    return current_number
+
+
+print(smallest(62524))
