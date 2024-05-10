947fe78a290c1d836b891b224ae5a42852408f56
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-05-10 10:11:55 +0300

    10.05.2024

--- find_the_smallest.py
+++ find_the_smallest.py
@@ -1,23 +1,7 @@
 def smallest(n):
-    number_as_str_list = [n for n in str(n)]
-    num_copy = number_as_str_list.copy()
-
-    smallest_single_digi = min([int(n) for n in number_as_str_list])
-
-    temp_smallest = n
-    temp_smallest_digi_index = None
-
-    for index, digi in enumerate(number_as_str_list):
-        if int(digi) == smallest_single_digi:
-            extract = num_copy.pop(index)
-            num_copy.insert(0, extract)
-            new_value_as_int = int("".join(num_copy))
-            if new_value_as_int < n:
-                temp_smallest = new_value_as_int
-                temp_smallest_digi_index = index
-            num_copy = number_as_str_list
-
-    return temp_smallest, temp_smallest_digi_index, 0
+    smallest_num = n
+    string_number = str(n)
+    ran = range(len(string_number))
 
 
 print(smallest(209917))
