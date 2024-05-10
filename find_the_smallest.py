852a70cf0ba5f74d4126ee315dbc48c5786a6cab
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-05-10 10:15:54 +0300

    10.05.2024

--- find_the_smallest.py
+++ find_the_smallest.py
@@ -2,6 +2,18 @@ def smallest(n):
     smallest_num = n
     string_number = str(n)
     ran = range(len(string_number))
-
+    x = None
+    y = None
+    for i in ran:
+        for j in ran:
+            temp_list = list(string_number)
+            index_var = temp_list.pop(i)
+            temp_list.insert(j, index_var)
+            result = int("".join(temp_list))
+            if result < smallest_num:
+                smallest_num = result
+                x = i
+                y = j
+    return [smallest_num, x, y]
 
 print(smallest(209917))
