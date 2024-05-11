19b36275c7bc2268c31dcf41d16c947aed10f008
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-05-11 22:03:34 +0300

    12.05.2024

--- count_ip_addresses.py
+++ count_ip_addresses.py
@@ -5,10 +5,10 @@ def ips_between(start, end):
     first_sum_split = [int(x) for x in first_sum_split]
     second_sum_split = [int(x) for x in second_sum_split]
 
-    first_sum = (256 * sum(first_sum_split[:-1])) + first_sum_split[-1]
-    second_sum = (256 * sum(second_sum_split[:-1])) + second_sum_split[-1]
+    first_sum = (first_sum_split[0] * (256 ** 3)) + (first_sum_split[1] * (256 ** 2)) + (first_sum_split[2] * 256) + first_sum_split[3]
+    second_sum = (second_sum_split[0] * (256 ** 3)) + (second_sum_split[1] * (256 ** 2)) + (second_sum_split[2] * 256) + second_sum_split[3]
 
     return second_sum - first_sum
 
 
-print(ips_between("20.0.0.10", "20.0.1.0"))
+print(ips_between("176.22.241.151", "176.23.12.7"))
