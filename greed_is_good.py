f946409a9efd98315455054bd411beffd03d5a89
aefremov2 <efremov.alex@yahoo.bg>
2024-05-13 05:34:11 +0300

    13.05.2024

--- greed_is_good.py
+++ greed_is_good.py
@@ -12,7 +12,25 @@ def score(dice):
     total_amount = 0
     dice_str = sorted(map(str, dice))
 
-    return dice_str
+    first_triplet = dice_str[:3]
+    second_part = dice_str[3:]
+
+    first_triplet_as_str = "".join(first_triplet)
+
+    for key, value in score_key.items():
+        if first_triplet_as_str == key:
+            total_amount += value
+        else:
+            for el in first_triplet:
+                if el == key:
+                    total_amount += value
+
+    for key, value in score_key.items():
+        for el in second_part:
+            if el == key:
+                total_amount += value
+
+    return second_part
 
 
 print(score([5, 1, 3, 4, 1]))
