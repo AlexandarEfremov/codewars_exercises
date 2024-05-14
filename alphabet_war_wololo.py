8c9ebb999ddad6c2454689dfbb486dbaf36b8005
aefremov2 <efremov.alex@yahoo.bg>
2024-05-14 08:43:40 +0300

    14.05.2024

--- alphabet_war_wololo.py
+++ alphabet_war_wololo.py
@@ -1,34 +1,17 @@
 def alphabet_war(fight):
-    string_as_list = [x for x in fight]
+    conversion_dict = {'j': {'w': 'm', 'p': 'q', 'b': 'd', 's': 'z'}, 't': {'m': 'w', 'q': 'p', 'd': 'b', 'z': 's'}}
 
-    left = {
-        "w": 4,
-        "p": 3,
-        "b": 2,
-        "s": 1,
-        "t": 0,
-    }
+    def return_opposite(char, position):
+        try:
+            two_positions_forward = string_as_list[position + 2]
+            if two_positions_forward != "t":
+                if string_as_list[index + 1]:
+                    new_char = opposites[string_as_list[position + 1]]
+        except IndexError:
+            pass
 
-    right = {
-        "m": 4,
-        "q": 3,
-        "d": 2,
-        "z": 1,
-        "j": 0,
-    }
+    for index, letter in enumerate(string_as_list):
 
-    opposites = {
-        "w": "m",
-        "p": "q",
-        "b": "d",
-        "s": "z",
-        "z": "s",
-        "d": "b",
-        "q": "p",
-        "m": "w",
-    }
-    for letter in string_as_list:
-        if letter == "j" or letter == "t":
 
 
     return "Let's fight again!"
