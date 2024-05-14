2f34514bd8ab4584ce4820ff0eca1bf797c8d045
aefremov2 <efremov.alex@yahoo.bg>
2024-05-14 07:50:15 +0300

    14.05.2024

--- alphabet_war_wololo.py
+++ alphabet_war_wololo.py
@@ -1,4 +1,21 @@
 def alphabet_war(fight):
+    left = {
+        "w": 4,
+        "p": 3,
+        "b": 2,
+        "s": 1,
+        "t": 0,
+    }
+
+    right = {
+        "m": 4,
+        "q": 3,
+        "d": 2,
+        "z": 1,
+        "j": 0,
+    }
+    str_len = len(fight)
+
     return "Let's fight again!"
 
 
