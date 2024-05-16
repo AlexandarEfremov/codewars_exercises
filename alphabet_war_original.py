4f8ce7137196a10ff3deffe5048d1af3a2bc163b
aefremov2 <efremov.alex@yahoo.bg>
2024-05-16 05:47:04 +0300

    16.05.2024

---
+++ alphabet_war_original.py
@@ -0,0 +1,12 @@
+def alphabet_war(fight):
+    initial_score = 0
+    alpha_dict = {
+                   "w": 4,
+                   "p": 3,
+                   "b": 2,
+                   "s": 1,
+                   "m": -4,
+                   "q": -3,
+                   "d": -2,
+                   "z": -1
+                   }
